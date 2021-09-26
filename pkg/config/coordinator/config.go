4633f9ea3eccc9a757d381c6aa38b54ad171fab2
Sergey Stepanov <sergystepanov@gmail.com>
2021-09-26 22:46:23 +0300

    Fmt

--- pkg/config/coordinator/config.go
+++ pkg/config/coordinator/config.go
@@ -19,12 +19,12 @@ type Config struct {
 
 type Coordinator struct {
 	RoundRobin bool
-		DebugHost  string
-		Library    games.Config
-		Monitoring monitoring.Config
-		Server     shared.Server
-		Analytics  Analytics
-	}
+	DebugHost  string
+	Library    games.Config
+	Monitoring monitoring.Config
+	Server     shared.Server
+	Analytics  Analytics
+}
 
 // Analytics is optional Google Analytics
 type Analytics struct {
